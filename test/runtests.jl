3dc0fef51213be9f132836392145cb3b4848477d
Mattias Villani <mattias.villani@gmail.com>
2026-01-06 22:20:40 +0100

    running two rather boring tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,19 @@
-using DynamicGlobalLocalShrinkage
 using Test
+using LinearAlgebra
+using DynamicGlobalLocalShrinkage
 
 @testset "DynamicGlobalLocalShrinkage.jl" begin
-    # Write your tests here.
+    T = 10; p = 4;
+    Σᵥ = [zeros(p,p) for t in 1:T];
+    H = zeros(T,p)
+    LogVol2Covs!(Σᵥ, H)
+
+    @test Σᵥ[1] == I(p)
+
+    offset = eps()*ones(T,p)
+    offSetMethod = "kowal"
+    ν = randn(T,p)
+    ν[:,1] .= eps()
+    setOffset!(offset, ν, offSetMethod)
+    @test all(offset[:,1] .> eps())
 end
