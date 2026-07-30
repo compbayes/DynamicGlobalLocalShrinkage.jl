12eeaaba9255dfde33b477eda477b106029f8bec
Mattias Villani <mattias.villani@gmail.com>
2026-07-30 14:23:18 +0200

    Defined T in univ homo sampler

--- src/HomoSamplers.jl
+++ src/HomoSamplers.jl
@@ -1,12 +1,13 @@
 """ 
-    update_homoscedastic_uni!(ν, H, ζ₀=4, λ²₀=exp(m₀ / 2))
+    update_homoscedastic_uni!(ν, H, ζ₀, λ²₀)
 
 Gibbs update of univariate homoscedastic variance instead of DSP
 using prior σ²ₖ ~ Inv-χ²(ζ₀, λ²₀)
 Still using the DSP container H, but all row are the same.
 """
-function update_homoscedastic_uni!(ν, H, ζ₀=4, λ²₀=exp(m₀ / 2))
-    for k = 1:size(H, 2)
+function update_homoscedastic_uni!(ν, H, ζ₀, λ²₀)
+    T, p = size(ν)
+    for k = 1:p
         ζₙ = ζ₀ + T
         λ²ₙ = (ζ₀ * λ²₀ + sum(ν[:, k] .^ 2)) / ζₙ
         H[:, k] .= log.(rand(ScaledInverseChiSq(ζₙ, λ²ₙ)))
